7bea6131682193f23b0a4e3126351b885998545d
Joao Ribeiro <joao.ribeiro@eproseed.com>
2018-11-09 11:20:46 +0000

    Added Player Movement and initial collision logging

---
+++ Assets/PlayerColision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerColision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log("We hit " + collisionInfo.collider.name);
+
+        if (collisionInfo.collider.name.Contains("Cube"))
+        {
+            Debug.Log("You Lost");
+        }
+    }
+
+}
