5699d466ee595aaa2e387b858d693635e702152a
Joao Ribeiro <joao.ribeiro@eproseed.com>
2018-11-09 13:08:47 +0000

    Project commited again to detect gitignore

---
+++ Assets/Collision/PlayerColision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerColision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            Debug.Log("You Lost");
+        }
+    }
+
+}
