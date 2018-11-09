13d456fa43427339b87f0f96864b4b8008615b60
Joao Ribeiro <joao.ribeiro@eproseed.com>
2018-11-09 10:19:13 +0000

    Initial commit with functional unity project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rg;
+
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("a"))
+        {
+            rg.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rg.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+
+    
+}
